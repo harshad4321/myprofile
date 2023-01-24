0441060cb7514e257c2d0e25cab8af39ea60cd9b
Harshad.N <harshad.n4321@gmail.com>
2023-01-24 23:34:36 +0530

    hbs is set

--- app.js
+++ app.js
@@ -7,17 +7,42 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var hbs = require('express-handlebars')
+const HBS = hbs.create({});
+var Handlebars = require('handlebars');
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials/' }));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+Handlebars.registerHelper("inc", function (value, options) {
+  return parseInt(value) + 1;
+});
+HBS.handlebars.registerHelper("ifCondition", function (v1, v2, options) {
+  if (v1 == v2) {
+    return options.fn(this)
+  }
+  return options.inverse(this)
+})
+
+HBS.handlebars.registerHelper("notEquals", function (v1, v2, options) {
+  if (v1 != v2) {
+    return options.fn(this)
+  }
+  return options.inverse(this)
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
