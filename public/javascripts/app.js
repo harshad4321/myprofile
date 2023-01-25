299a3e81362d4a4b777d81a87514477d86380547
Harshad.N <harshad.n4321@gmail.com>
2023-01-25 12:40:57 +0530

    serting app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -62,3 +62,36 @@ class Dot {
     ctx.fillRect(this.pos.x - this.mass, this.pos.y - this.mass, this.mass * 2, this.mass * 2)
   }
 }
+
+class Stick {
+  constructor(p1, p2) {
+    this.startPoint = p1
+    this.endPoint = p2
+
+    this.length = this.startPoint.pos.dist(this.endPoint.pos)
+    this.tension = 0.3
+  }
+
+  update() {
+    const dx = this.endPoint.pos.x - this.startPoint.pos.x
+    const dy = this.endPoint.pos.y - this.startPoint.pos.y
+
+    const dist = Math.sqrt(dx * dx + dy * dy)
+    const diff = (dist - this.length) / dist
+
+    const offsetX = diff * dx * this.tension
+    const offsetY = diff * dy * this.tension
+
+    const m = this.startPoint.mass + this.endPoint.mass
+    const m1 = this.endPoint.mass / m
+    const m2 = this.startPoint.mass / m
+
+    if (!this.startPoint.pinned) {
+      this.startPoint.pos.x += offsetX * m1
+      this.startPoint.pos.y += offsetY * m1
+    }
+    if (!this.endPoint.pinned) {
+      this.endPoint.pos.x -= offsetX * m2
+      this.endPoint.pos.y -= offsetY * m2
+    }
+  }
