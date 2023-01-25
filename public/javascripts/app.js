7dbe8588dc5a235f6cb8f1adaf717e2b8ac80d68
Harshad.N <harshad.n4321@gmail.com>
2023-01-25 12:41:57 +0530

    serting app.js is set

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -95,3 +95,195 @@ class Stick {
       this.endPoint.pos.y -= offsetY * m2
     }
   }
+
+  draw(ctx) {
+    ctx.beginPath()
+    ctx.strokeStyle = '#999'
+    ctx.moveTo(this.startPoint.pos.x, this.startPoint.pos.y)
+    ctx.lineTo(this.endPoint.pos.x, this.endPoint.pos.y)
+    ctx.stroke()
+    ctx.closePath()
+  }
+}
+
+class Rope {
+  constructor(config) {
+    this.x = config.x
+    this.y = config.y
+    this.segments = config.segments || 10
+    this.gap = config.gap || 15
+    this.color = config.color || 'gray'
+
+    this.dots = []
+    this.sticks = []
+
+    this.iterations = 10
+
+    this.create()
+  }
+
+  pin(index) {
+    this.dots[index].pinned = true
+  }
+
+  create() {
+    for (let i = 0; i < this.segments; i++) {
+      this.dots.push(new Dot(this.x, this.y + i * this.gap))
+    }
+    for (let i = 0; i < this.segments - 1; i++) {
+      this.sticks.push(new Stick(this.dots[i], this.dots[i + 1]))
+    }
+  }
+
+  update(mouse) {
+    this.dots.forEach(dot => {
+      dot.update(mouse)
+    })
+    for (let i = 0; i < this.iterations; i++) {
+      this.sticks.forEach(stick => {
+        stick.update()
+      })
+    }
+  }
+
+  draw(ctx) {
+    this.dots.forEach(dot => {
+      dot.draw(ctx)
+    })
+    this.sticks.forEach(stick => {
+      stick.draw(ctx)
+    })
+    this.dots[this.dots.length - 1].drawLight(ctx)
+  }
+}
+
+class App {
+  static width = innerWidth
+  static height = innerHeight
+  static dpr = devicePixelRatio > 1 ? 2 : 1
+  static interval = 1000 / 60
+
+  constructor() {
+    this.canvas = document.querySelector('canvas')
+    this.ctx = this.canvas.getContext('2d')
+
+    this.mouse = new Mouse(this.canvas)
+
+    this.resize()
+    window.addEventListener('resize', this.resize.bind(this))
+
+    this.createRopes()
+  }
+
+  createRopes() {
+    this.ropes = []
+
+    const TOTAL = App.width * 0.06
+    for (let i = 0; i < TOTAL + 1; i++) {
+      const x = randomNumBetween(App.width * 0.3, App.width * 0.7)
+      const y = 0
+      const gap = randomNumBetween(App.height * 0.05, App.height * 0.08)
+      const segments = 10
+      const rope = new Rope({ x, y, gap, segments })
+      rope.pin(0)
+
+      this.ropes.push(rope)
+    }
+  }
+
+  resize() {
+    App.width = innerWidth
+    App.height = innerHeight
+
+    this.canvas.style.width = '100%'
+    this.canvas.style.height = '100%'
+    this.canvas.width = App.width * App.dpr
+    this.canvas.height = App.height * App.dpr
+    this.ctx.scale(App.dpr, App.dpr)
+
+    this.createRopes()
+  }
+
+  render() {
+    let now, delta
+    let then = Date.now()
+
+    const frame = () => {
+      requestAnimationFrame(frame)
+      now = Date.now()
+      delta = now - then
+      if (delta < App.interval) return
+      then = now - (delta % App.interval)
+      this.ctx.clearRect(0, 0, App.width, App.height)
+
+      // draw here
+      this.ropes.forEach(rope => {
+        rope.update(this.mouse)
+        rope.draw(this.ctx)
+      })
+    }
+    requestAnimationFrame(frame)
+  }
+}
+
+function randomNumBetween(min, max) {
+  return Math.random() * (max - min) + min
+}
+
+window.addEventListener('load', () => {
+  const app = new App()
+  app.render()
+})
+
+class Vector {
+  constructor(x, y) {
+    this.x = x || 0
+    this.y = y || 0
+  }
+  static add(v1, v2) {
+    return new Vector(v1.x + v2.x, v1.y + v2.y)
+  }
+  static sub(v1, v2) {
+    return new Vector(v1.x - v2.x, v1.y - v2.y)
+  }
+  add(x, y) {
+    if (arguments.length === 1) {
+      this.x += x.x
+      this.y += x.y
+    } else if (arguments.length === 2) {
+      this.x += x
+      this.y += y
+    }
+    return this
+  }
+  sub(x, y) {
+    if (arguments.length === 1) {
+      this.x -= x.x
+      this.y -= x.y
+    } else if (arguments.length === 2) {
+      this.x -= x
+      this.y -= y
+    }
+    return this
+  }
+  mult(v) {
+    if (typeof v === 'number') {
+      this.x *= v
+      this.y *= v
+    } else {
+      this.x *= v.x
+      this.y *= v.y
+    }
+    return this
+  }
+  setXY(x, y) {
+    this.x = x
+    this.y = y
+    return this
+  }
+  dist(v) {
+    const dx = this.x - v.x
+    const dy = this.y - v.y
+    return Math.sqrt(dx * dx + dy * dy)
+  }
+}
