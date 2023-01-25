e2092b90f6ca6b8887d33d4f7a14e4849b056018
Harshad.N <harshad.n4321@gmail.com>
2023-01-25 12:39:58 +0530

    serting app.ja

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -25,4 +25,40 @@ class Dot {
     this.lightImg = document.querySelector('#light-img')
     this.lightSize = 15;
   }
+
+  update(mouse) {
+    if (this.pinned) return
+
+    let vel = Vector.sub(this.pos, this.oldPos)
+
+    this.oldPos.setXY(this.pos.x, this.pos.y)
+
+    vel.mult(this.friction)
+    vel.add(this.gravity)
+
+    let { x: dx, y: dy } = Vector.sub(mouse.pos, this.pos)
+    const dist = Math.sqrt(dx * dx + dy * dy)
+
+    const direction = new Vector(dx / dist, dy / dist)
+
+    const force = Math.max((mouse.radius - dist) / mouse.radius, 0)
+
+    if (force > 0.6) this.pos.setXY(mouse.pos.x, mouse.pos.y)
+    else {
+      this.pos.add(vel)
+      this.pos.add(direction.mult(force))
+    }
+  }
+
+  drawLight(ctx) {
+    ctx.drawImage(
+      this.lightImg,
+      this.pos.x - this.lightSize / 2, this.pos.y - this.lightSize / 2, this.lightSize, this.lightSize
+    )
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = '#aaa'
+    ctx.fillRect(this.pos.x - this.mass, this.pos.y - this.mass, this.mass * 2, this.mass * 2)
+  }
 }
